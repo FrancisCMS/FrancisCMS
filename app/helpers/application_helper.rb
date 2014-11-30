98c2eef395f2ff5b40d75e8fa382f506613921d7
Jason Garber <jason@sixtwothree.org>
2014-11-29 19:44:08 -0500

    Add page titles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_description
+    @page_description || Settings.site.description
+  end
+
+  def page_title
+    return "#{@page_title} – #{Settings.site.title}" if @page_title
+    "#{Settings.site.title} – #{Settings.site.description}"
+  end
 end
