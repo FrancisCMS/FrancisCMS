b9fbd7fd1c6312803d16eae1b480fa08f5b2f58b
Jason Garber <jason@sixtwothree.org>
2014-12-01 13:12:59 -0500

    Re-work tag show page to combine results, ordered reverse chronologically.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,12 +6,14 @@ class TagsController < ApplicationController
   def show
     tag
 
-    @posts = Post.tagged_with(@tag)
-    @links = Link.tagged_with(@tag)
+    posts = Post.tagged_with(@tag).exclude_drafts
+    links = Link.tagged_with(@tag).exclude_drafts
+    @results = (posts + links).sort_by { |result| result[:published_at] }.reverse
 
-    unless logged_in?
-      @posts = Post.tagged_with(@tag).exclude_drafts
-      @links = Link.tagged_with(@tag).exclude_drafts
+    if logged_in?
+      posts = Post.tagged_with(@tag)
+      links = Link.tagged_with(@tag)
+      @results = (posts + links).sort_by { |result| result[:created_at] }.reverse
     end
   end
 
