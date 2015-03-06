2acc5197d845fd3575bbed8ec2f9f062af5a2ca8
Jason Garber <jason@sixtwothree.org>
2015-03-06 16:19:08 -0500

    Include engine's migrations when parent app runs db:migrate.

--- lib/francis_cms/engine.rb
+++ lib/francis_cms/engine.rb
@@ -32,5 +32,13 @@ module FrancisCms
       app.config.francis_cms.license_title    = 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International'
       app.config.francis_cms.license_url      = 'http://creativecommons.org/licenses/by-nc-sa/4.0/'
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
   end
 end
