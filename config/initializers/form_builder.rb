99cfbad6fdf51c481a9a88a543c9352a22bbedd6
Jason Garber <jason@sixtwothree.org>
2015-05-07 19:07:12 -0400

    Add form builder with support for aria-describedby.

---
+++ config/initializers/form_builder.rb
@@ -0,0 +1,23 @@
+class FrancisCmsFormBuilder < ActionView::Helpers::FormBuilder
+  def text_area(method, options = {})
+    super(method, add_aria_options(@object, method, options))
+  end
+
+  def text_field(method, options = {})
+    super(method, add_aria_options(@object, method, options))
+  end
+
+  def url_field(method, options = {})
+    super(method, add_aria_options(@object, method, options))
+  end
+
+  private
+
+  def add_aria_options(object, method, options)
+    if object.errors.include?(method)
+      { :'aria-describedby' => %{#{object.class.name.demodulize.downcase}_#{method}_errors} }.merge!(options)
+    else
+      options
+    end
+  end
+end
