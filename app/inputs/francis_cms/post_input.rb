aa7d824142e9dc10ba5b800d76a56b4d1f327bcc
Jason Garber <jason@sixtwothree.org>
2015-07-11 17:26:51 -0400

    Refactor post_input.

--- app/inputs/francis_cms/post_input.rb
+++ app/inputs/francis_cms/post_input.rb
@@ -1,19 +1,15 @@
 module FrancisCms
   class PostInput
-    def initialize(params)
-      @params = params
-    end
-
-    def to_h
-      Hash[
-        attribute_keys.zip(@params.fetch(:post, {}).values_at(*attribute_keys))
-      ]
-    end
+    include FrancisCms::Input
 
     private
 
     def attribute_keys
       [:title, :slug, :body, :excerpt, :tag_list, :is_draft]
+    end
+
+    def type_key
+      :post
     end
   end
 end
