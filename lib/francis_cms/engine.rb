1d8bce740398b73645ee20f35d56e4280bf8a6ea
Jason Garber <jason@sixtwothree.org>
2015-08-09 16:56:44 -0400

    Add some notes to configuration settings.

--- lib/francis_cms/engine.rb
+++ lib/francis_cms/engine.rb
@@ -18,16 +18,16 @@ module FrancisCms
       app.config.francis_cms = ActiveSupport::OrderedOptions.new
 
       app.config.francis_cms.logged_in_method = :logged_in?
-      app.config.francis_cms.login_path       = '/login'
+      app.config.francis_cms.login_path       = '/login' # path may be relative or absolute
 
-      app.config.francis_cms.site_url         = 'http://example.com/'
+      app.config.francis_cms.site_url         = 'http://example.com/' # `site_url` must include protocol and trailing slash
       app.config.francis_cms.site_title       = 'FrancisCMS Demo Site'
       app.config.francis_cms.site_description = 'This is the default site description for a new FrancisCMS-powered website.'
       app.config.francis_cms.site_language    = 'en-US'
 
       app.config.francis_cms.user_name        = 'Jane Doe'
       app.config.francis_cms.user_email       = 'jane@example.com'
-      app.config.francis_cms.user_avatar      = 'http://www.placecage.com/180/180'
+      app.config.francis_cms.user_avatar      = 'http://www.placecage.com/180/180' # path may be relative or absolute
 
       app.config.francis_cms.license_title    = 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International'
       app.config.francis_cms.license_url      = 'http://creativecommons.org/licenses/by-nc-sa/4.0/'
