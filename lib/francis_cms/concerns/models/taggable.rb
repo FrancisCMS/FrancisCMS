81ef4ce67c109f6d586df9ef6c56e9e68d77f2f1
Jason Garber <jason@sixtwothree.org>
2016-01-24 11:37:55 -0500

    Add sorted_tags method to taggable concern and update views and partial.

--- lib/francis_cms/concerns/models/taggable.rb
+++ lib/francis_cms/concerns/models/taggable.rb
@@ -2,6 +2,10 @@ module FrancisCms::Concerns::Models::Taggable
   extend ActiveSupport::Concern
 
   included do
-    acts_as_ordered_taggable
+    acts_as_taggable
+  end
+
+  def sorted_tags
+    tags.sort_by { |tag| tag.name.downcase }
   end
 end
