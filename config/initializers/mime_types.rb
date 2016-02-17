654155ebf3da20d87a3c6e40d409e6c6335d8995
Jason Garber <jason@sixtwothree.org>
2016-02-17 00:05:43 -0500

    Serve RSS as application/xml.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,1 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext", :rtf
+
+Mime::Type.unregister(:rss)
+Mime::Type.register 'application/xml', :rss
 Mime::Type.register 'text/x-markdown', :md
