69692a8fb9207ddc2d13db31ba0114522d94e48e
Jason Garber <jason@sixtwothree.org>
2016-03-22 20:44:22 -0400

    Use File.read to grab contents of default avatar. Rails.application.assets is unavailable in production.

--- app/helpers/francis_cms/application_helper.rb
+++ app/helpers/francis_cms/application_helper.rb
@@ -3,7 +3,7 @@ module FrancisCms::ApplicationHelper
     if obj.author_avatar.present?
       raw image_tag(obj.author_avatar_url(:small), html_options.merge({ srcset: %{#{asset_path obj.author_avatar_url(:large)} 2x} }))
     else
-      raw image_tag(%{data:image/png;base64,#{Base64.encode64 Rails.application.assets['avatar.png'].to_s}}, html_options)
+      raw image_tag(%{data:image/png;base64,#{Base64.encode64 File.read(FrancisCms::Engine.root.join('app/assets/images/avatar.png'))}}, html_options)
     end
   end
 end
