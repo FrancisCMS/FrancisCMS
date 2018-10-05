0a878ff4a7ef47165ce10208a9789f759f6b849a
Jason Garber <jason@sixtwothree.org>
2018-10-04 20:37:28 -0400

    Revert conditional logic bug introduced in 0d5ee8c

--- app/validators/francis_cms/webmention_validator.rb
+++ app/validators/francis_cms/webmention_validator.rb
@@ -1,13 +1,9 @@
 module FrancisCms
   class WebmentionValidator < ActiveModel::Validator
     def validate(record)
-      return record.errors[:base] << 'Webmention source and target must be different.' if source_is_target?
-    end
-
-    private
-
-    def source_is_target?
-      record.source.sub(%r{^https?://}, '') == record.target.sub(%r{^https?://}, '')
+      if record.source.sub(/^https?:\/\//, '') == record.target.sub(/^https?:\/\//, '')
+        record.errors[:base] << 'Webmention source and target must be different.'
+      end
     end
   end
 end
