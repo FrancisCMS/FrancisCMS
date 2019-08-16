a58f4cb91995ef04d0a3c279562d76e7dd6e1f6d
Jason Garber <jason@sixtwothree.org>
2019-08-15 22:16:06 -0400

    Pass around record

--- app/validators/francis_cms/webmention_validator.rb
+++ app/validators/francis_cms/webmention_validator.rb
@@ -1,12 +1,12 @@
 module FrancisCms
   class WebmentionValidator < ActiveModel::Validator
     def validate(record)
-      record.errors[:base] << 'Webmention source and target must be different.' if source_is_target?
+      record.errors[:base] << 'Webmention source and target must be different.' if source_is_target?(record)
     end
 
     private
 
-    def source_is_target?
+    def source_is_target?(record)
       record.source.sub(%r{^https?://}, '') == record.target.sub(%r{^https?://}, '')
     end
   end
