d4af56ed00a98f9cfd65785481be87218d2a1f9d
Jason Garber <jason@sixtwothree.org>
2019-11-17 21:25:50 -0500

    Remove custom RSS mime configuration

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,6 +3,4 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 
-Mime::Type.unregister(:rss)
-Mime::Type.register 'application/xml', :rss
 Mime::Type.register 'text/markdown', :md
