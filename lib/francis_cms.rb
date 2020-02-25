91d31182a156302d75918dd6fdb244cd0e4520b3
Jason Garber <jason@sixtwothree.org>
2020-02-25 10:05:14 -0500

    Disable Style/AccessModifierDeclarations cop

--- lib/francis_cms.rb
+++ lib/francis_cms.rb
@@ -14,10 +14,12 @@ require 'twitter'
 require 'will_paginate'
 
 module FrancisCms
+  # rubocop:disable Style/AccessModifierDeclarations
   def configuration
     Rails.application.config.francis_cms
   end
   module_function :configuration
+  # rubocop:enable Style/AccessModifierDeclarations
 end
 
 require 'francis_cms/engine'
