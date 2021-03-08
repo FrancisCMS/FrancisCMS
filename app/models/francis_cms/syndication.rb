37700e9a315946e96b6866d6cd6e26f0dd70d347
Jason Garber <jason@sixtwothree.org>
2021-03-07 20:11:49 -0500

    Use URI::DEFAULT_PARSER.make_regexp instead of URI.regexp

--- app/models/francis_cms/syndication.rb
+++ app/models/francis_cms/syndication.rb
@@ -3,7 +3,7 @@ module FrancisCms
     belongs_to :syndicatable, polymorphic: true
 
     validates :url, :name, presence: true
-    validates :url, uniqueness: true, format: { with: URI.regexp(%w[http https]) }
+    validates :url, uniqueness: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
 
     default_scope { order('name ASC') }
   end
